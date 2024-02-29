c53f8f2a8b803c7674cebcdac9d6a59785d86c0e
Yuyuko1024 <dingwenxuan4@gmail.com>
2024-02-29 21:57:53 +0800

    Use default stream channel

--- app/src/main/java/net/hearnsoft/gensokyoradio/trd/utils/Constants.java
+++ app/src/main/java/net/hearnsoft/gensokyoradio/trd/utils/Constants.java
@@ -8,8 +8,8 @@ public class Constants {
 
     public static final String NOW_PLAYING_JSON = "https://gensokyoradio.net/api/station/playing/";
 
-    //public static final String GR_STREAM_URL = "https://stream.gensokyoradio.net/1/";
-    public static final String GR_STREAM_URL = "http://106.14.246.83:8000/stream";
+    public static final String GR_STREAM_URL = "https://stream.gensokyoradio.net/1/";
+
     public static final String GR_STREAM_URL_EU1 = "http://eu1.stream.gensokyoradio.net:8000/1/";
     public static final String NOTIFICATION_CHANNEL_ID = "PlayChannel";
 
