34f80e93849cf04ca97760d5179e709949f875de
Yuyuko1024 <dingwenxuan4@gmail.com>
2024-03-03 11:12:45 +0800

    Remove unused notification channel id

--- app/src/main/java/net/hearnsoft/gensokyoradio/trd/utils/Constants.java
+++ app/src/main/java/net/hearnsoft/gensokyoradio/trd/utils/Constants.java
@@ -11,6 +11,5 @@ public class Constants {
     public static final String GR_STREAM_URL = "https://stream.gensokyoradio.net/1/";
 
     public static final String GR_STREAM_URL_EU1 = "http://eu1.stream.gensokyoradio.net:8000/1/";
-    public static final String NOTIFICATION_CHANNEL_ID = "PlayChannel";
 
 }
