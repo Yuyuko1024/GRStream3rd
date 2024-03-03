79b47378904ccb7689f2f26d9b36f1bea0ade514
Yuyuko1024 <dingwenxuan4@gmail.com>
2024-03-03 11:17:40 +0800

    Set global shared preference name

--- app/src/main/java/net/hearnsoft/gensokyoradio/trd/utils/Constants.java
+++ app/src/main/java/net/hearnsoft/gensokyoradio/trd/utils/Constants.java
@@ -12,4 +12,6 @@ public class Constants {
 
     public static final String GR_STREAM_URL_EU1 = "http://eu1.stream.gensokyoradio.net:8000/1/";
 
+    public static final String PREF_GLOBAL_NAME = "gr_3rd";
+
 }
