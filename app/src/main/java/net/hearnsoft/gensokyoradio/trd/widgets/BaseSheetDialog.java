c8d5971897bd6f093204bd2a76141d35c740f38e
Yuyuko1024 <dingwenxuan4@gmail.com>
2024-07-13 15:15:26 +0800

    Rewrite BaseSheetDialog code
     * Set device portrait display width as
       default width
     * Only expand dialog when device is landscape

--- app/src/main/java/net/hearnsoft/gensokyoradio/trd/widgets/BaseSheetDialog.java
+++ app/src/main/java/net/hearnsoft/gensokyoradio/trd/widgets/BaseSheetDialog.java
@@ -1,18 +1,69 @@
 package net.hearnsoft.gensokyoradio.trd.widgets;
 
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.res.Configuration;
+import android.graphics.Point;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
 
 import androidx.annotation.NonNull;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
 
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 
 import net.hearnsoft.gensokyoradio.trd.R;
 
 public abstract class BaseSheetDialog extends BottomSheetDialogFragment {
 
+    private BottomSheetBehavior<FrameLayout> behavior;
+
     @Override
     public int getTheme() {
         return R.style.BottomSheetDialogTheme;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        getDialog().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+        BottomSheetDialog dialog = (BottomSheetDialog) getDialog();
+        FrameLayout bottomSheet = dialog.getDelegate()
+                .findViewById(com.google.android.material.R.id.design_bottom_sheet);
+        if (bottomSheet != null) {
+            CoordinatorLayout.LayoutParams params = (CoordinatorLayout.LayoutParams) bottomSheet.getLayoutParams();
+            params.width = getWidth();
+
+            behavior = BottomSheetBehavior.from(bottomSheet);
+            if (getContext().getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                behavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+            } else {
+                behavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+            }
+        }
+    }
+
+    private int getWidth() {
+        int width = 1080;
+        if (getContext() != null) {
+            WindowManager windowManager = (WindowManager) getContext().getSystemService(Context.WINDOW_SERVICE);
+            Point point = new Point();
+            if (windowManager != null) {
+                windowManager.getDefaultDisplay().getSize(point);
+                if (getContext().getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+                    width = point.x;
+                } else if (getContext().getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                    width = point.y;
+                }
+            }
+        }
+        return width;
+    }
+
+    public BottomSheetBehavior<FrameLayout> getBehavior() {
+        return behavior;
     }
 
     @Override
